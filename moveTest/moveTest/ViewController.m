1ddfdd6a7decaaee700d140d20b32e96129b8c05
杨小兵 <six_tiger@163.com>
2015-06-24 17:57:59 +0800

    新增实例动画

--- moveTest/moveTest/ViewController.m
+++ moveTest/moveTest/ViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
     self.moviewView.backgroundColor = [UIColor grayColor];
     NSMutableArray  *dataArray = [NSMutableArray array];
-    for ( int i = 0; i < 21; i ++)
+    for ( int i = 0; i < 61; i ++)
     {
         [dataArray addObject:[[XXBMoveCellModel alloc] initWithIndex:i]];
     }
